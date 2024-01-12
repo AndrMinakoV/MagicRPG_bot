b2c28b3ab053cdea29630004b8bff71768159a06
AndrMinakoV <borishasanov591@gmail.com>
2024-01-12 17:51:36 +0200

    nam pizda poka ya daun

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -5,52 +5,33 @@ import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import org.slf4j.Logger;
-import com.mojang.logging.LogUtils;
+import org.slf4j.LoggerFactory;
+@Mod(ExampleMod.MOD_ID)
+public class ExampleMod {
+    public static final String MOD_ID = "examplemod";
+    private static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-import javax.security.auth.login.LoginException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ExecutorService;
-
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
-{
-    public static final String MODID = "examplemod";
-    private static final Logger LOGGER = LogUtils.getLogger();
-    private final ExecutorService pool = Executors.newSingleThreadExecutor();
-
-    public ExampleMod()
-
-    {
-        startDiscordBot();
-
-        // Register ourselves for server and other game events we are interested in
+    public ExampleMod() {
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
-        // Initialization code for your mod
-        LOGGER.info("HELLO FROM COMMON SETUP");
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ModListeners {
+
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            LOGGER.info("test_onClientSetup");
+            startDiscordBot();
+        }
     }
 
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
-        // Code to execute when the server starts
-        LOGGER.info("HELLO from server starting");
-
-    }
-
-    private void startDiscordBot() {
-
-            JDABuilder builder = JDABuilder.createDefault("");
-            builder.setActivity(Activity.watching("MagicRPG 1.19.2"));
-            builder.enableIntents(GatewayIntent.GUILD_MEMBERS);
-            builder.build();
-            LOGGER.info("Discord bot started successfully.");
+    private static void startDiscordBot() {
+        JDABuilder builder = JDABuilder.createDefault("");
+        builder.setActivity(Activity.watching("MagicRPG 1.19.2"));
+        builder.enableIntents(GatewayIntent.GUILD_MEMBERS);
+        builder.build();
+        LOGGER.info("Discord bot started successfully.");
 
     }
 }
